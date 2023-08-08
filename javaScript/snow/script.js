015240ed18f3c73d677e6cf48a5031b91fdb398e
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-08-08 12:54:28 -0700

    Snow Complete

--- javaScript/snow/script.js
+++ javaScript/snow/script.js
@@ -1,27 +1,6 @@
-// function creatSnow() {
-//     let snow = document.createElement('div')
-//     snow.classList.add('snow')
-
-//     snow.style.right = Math.random() * 100
-//         + 'vw'
-
-//     snow.style.animationDuration = Math.
-//         random() * 2 + 3 + 's'
-
-//     snow.innerText = '❄'
-//     document.body.appendChild(snow)
-
-//     setTimeout(() => {
-//         snow.remove()
-//     }, 5000)
-// }
-
-// setInterval(creatSnow, 100)
-
 function creatSnow() {
     let snow = document.createElement('div')
     snow.classList.add('snow')
-
 
     snow.style.right = Math.random() * 100
         + 'vw'
